534cff5931f1c5df6f12130ff26a10da232ad0b1
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-06-02 18:03:14 +0200

    changed build_env to npm script

---
+++ scripts/build_env.js
@@ -0,0 +1,17 @@
+const BANGSERVER_COMMIT_HASH_API = 'https://api.github.com/repos/salvoilmiosi/banggameserver/branches/release';
+const BANGSERVER_URL = 'ws://bang.salvoserver.it:47654';
+
+async function main () {
+    const response = await fetch(BANGSERVER_COMMIT_HASH_API);
+    const body = JSON.parse(await response.text());
+
+    const commitHash = body['commit']['sha'];
+
+    const result = `REACT_APP_BANG_SERVER_URL=${BANGSERVER_URL}\nREACT_APP_BANG_SERVER_COMMIT_HASH=${commitHash}`;
+    
+    console.log(result);
+
+    await require('fs').promises.writeFile('.env', result);
+}
+
+main();
