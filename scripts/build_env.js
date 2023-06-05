5bad3f56e6384b3c51bdfe42098d3d43a9501025
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-06-05 10:13:26 +0200

    remove junk

--- scripts/build_env.js
+++ scripts/build_env.js
@@ -2,16 +2,20 @@ const BANGSERVER_COMMIT_HASH_API = 'https://api.github.com/repos/salvoilmiosi/ba
 const BANGSERVER_URL = 'ws://bang.salvoserver.it:47654';
 
 async function main () {
-    const response = await fetch(BANGSERVER_COMMIT_HASH_API);
-    const body = JSON.parse(await response.text());
+    try {
+        const response = await fetch(BANGSERVER_COMMIT_HASH_API);
+        const body = JSON.parse(await response.text());
 
-    const commitHash = body['commit']['sha'];
+        const commitHash = body['commit']['sha'];
 
-    const result = `REACT_APP_BANG_SERVER_URL=${BANGSERVER_URL}\nREACT_APP_BANG_SERVER_COMMIT_HASH=${commitHash}`;
-    
-    console.log(result);
+        const result = `REACT_APP_BANG_SERVER_URL=${BANGSERVER_URL}\nREACT_APP_BANG_SERVER_COMMIT_HASH=${commitHash}`;
+        
+        console.log(result);
 
-    await require('fs').promises.writeFile('.env', result);
+        await require('fs').promises.writeFile('.env', result);
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 main();
