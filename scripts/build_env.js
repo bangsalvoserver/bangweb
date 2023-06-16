fdf8657d34d26370a733c1aee55a56989c60cfbb
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-06-16 15:31:11 +0200

    refactor build_env

--- scripts/build_env.js
+++ scripts/build_env.js
@@ -1,22 +1,25 @@
-const BANGSERVER_COMMIT_HASH_API = 'https://api.github.com/repos/salvoilmiosi/banggameserver/branches/release';
-const BANGSERVER_URL = 'ws://bang.salvoserver.it:47654';
-const BANGCARDS_URL = 'http://bang.salvoserver.it:81';
+async function getCommitHash(apiUrl) {
+    const response = await fetch(apiUrl);
+    const body = JSON.parse(await response.text());
+    return body['commit']['sha'];
+}
 
-async function main () {
+async function saveToFile(object, filename) {
+    const fs = require('fs').promises;
+    const result = Object.entries(object)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('\n');
+    console.log(result);
+    await fs.writeFile(filename, result);
+}
+
+async function main() {
     try {
-        const response = await fetch(BANGSERVER_COMMIT_HASH_API);
-        const body = JSON.parse(await response.text());
-
-        const commitHash = body['commit']['sha'];
-
-        const result =
-            `REACT_APP_BANG_CARDS_BASE_URL=${BANGCARDS_URL}\n` +
-            `REACT_APP_BANG_SERVER_URL=${BANGSERVER_URL}\n` +
-            `REACT_APP_BANG_SERVER_COMMIT_HASH=${commitHash}`;
-        
-        console.log(result);
-
-        await require('fs').promises.writeFile('.env', result);
+        await saveToFile({
+            REACT_APP_BANG_CARDS_BASE_URL: 'http://bang.salvoserver.it:81',
+            REACT_APP_BANG_SERVER_URL: 'ws://bang.salvoserver.it:47654',
+            REACT_APP_BANG_SERVER_COMMIT_HASH: await getCommitHash('https://api.github.com/repos/salvoilmiosi/banggameserver/branches/release')
+        }, '.env')
     } catch (e) {
         console.error(e);
     }
