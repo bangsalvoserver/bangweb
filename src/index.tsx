f4f8641aaa3bef036991f122cfb43f9e99e34f91
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-06-16 23:21:55 +0200

    set html tag lang property

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+document.documentElement.lang = (() => {
+  switch (navigator.language.toLowerCase()) {
+  case 'it-it': return 'it';
+  default: return 'en';
+  }
+})();
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <App />
