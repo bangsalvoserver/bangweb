76e88e98062c184775d5201134a279af29aae924
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-06-17 17:14:03 +0200

    fix build_env

--- scripts/build_env.js
+++ scripts/build_env.js
@@ -1,15 +1,15 @@
 async function getCommitHash(apiUrl) {
     const response = await fetch(apiUrl);
-    const body = JSON.parse(await response.text());
+    const body = await response.json();
     if ('commit' in body) {
         if ('sha' in body.commit) {
             return body.commit.sha;
         }
     }
-    throw Error('Cannot find commit hash');
+    throw new Error('Cannot find commit hash');
 }
 
-async function saveToFile(object, filename) {
+async function saveToFile(filename, object) {
     const fs = require('fs').promises;
     const result = Object.entries(object)
         .map(([key, value]) => `${key}=${value}`)
@@ -19,11 +19,11 @@ async function saveToFile(object, filename) {
 }
 
 async function main() {
-    await saveToFile({
+    await saveToFile('.env', {
         REACT_APP_BANG_CARDS_BASE_URL: 'http://bang.salvoserver.it:81',
         REACT_APP_BANG_SERVER_URL: 'ws://bang.salvoserver.it:47654',
         REACT_APP_BANG_SERVER_COMMIT_HASH: await getCommitHash('https://api.github.com/repos/salvoilmiosi/banggameserver/branches/release')
-    }, '.env');
+    });
 }
 
 main().catch(error => { throw error });
