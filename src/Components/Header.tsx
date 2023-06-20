22bdb07afbc5284fcd0abff1c4258065c165843d
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-06-20 16:01:41 +0200

    Add changing of username in UserMenu

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -71,7 +71,7 @@ function Header({ title, username, propic, editUser, handleLeaveLobby, handleDis
               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
             </button>
             { isMenuOpen &&
-              <UserMenu username={username}>
+              <UserMenu username={username} setUsername={username => editUser(username, propic)}>
                 { handleLeaveLobby && <UserMenuItem onClick={closeMenuAnd(handleLeaveLobby)}>{getLabel('ui', 'BUTTON_LEAVE_LOBBY')}</UserMenuItem> }
                 <UserMenuItem onClick={closeMenuAnd(handleDisconnect)}>{getLabel('ui', 'BUTTON_DISCONNECT')}</UserMenuItem>
               </UserMenu> }
--- src/Components/UserMenu.tsx
+++ src/Components/UserMenu.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
+import "./Style/UserMenu.css"
+import getLabel from "../Locale/GetLabel";
 
 export interface UserMenuItemProps {
   onClick: () => void;
@@ -11,17 +13,34 @@ export function UserMenuItem({ onClick, children }: UserMenuItemProps) {
 
 export interface UserMenuProps {
   username?: string;
+  setUsername: (value: string) => void;
   children: ReactNode;
 }
 
-export default function UserMenu({ username, children }: UserMenuProps) {
+export default function UserMenu({ username, setUsername, children }: UserMenuProps) {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const [showInput, setShowInput] = useState(false);
+
+  useEffect(() => {
+    if (showInput) {
+      usernameRef.current?.focus();
+    }
+  }, [showInput]);
+
   return (
-    <div style={{width: '10em'}} className='z-50
+    <div className='user-menu z-50
     absolute top-10 right-0
     text-base list-none divide-y rounded-lg shadow bg-gray-700 divide-gray-600'
     >
       <div className="px-4 py-3">
-        <span className="block text-sm text-white">{username}</span>
+        <div className={showInput ? 'username-input' : 'username-span'}
+          onClick={() => setShowInput(true)}>
+          <span className="block text-sm text-white">{username || getLabel('ui', 'UNKNOWN_PLAYER')}</span>
+          <input ref={usernameRef} value={username}
+            onChange={e => setUsername(e.target.value)}
+            onBlur={() => setShowInput(false)}
+          />
+        </div>
       </div>
       <ul className="py-2" aria-labelledby="user-menu-button">{children}</ul>
     </div>
