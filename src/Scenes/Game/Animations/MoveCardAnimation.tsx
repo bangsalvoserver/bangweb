123c6bce5bceb1cf9026f55d03c01b411acc3ceb
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-06-22 18:08:25 +0200

    fix clampCardRect

--- src/Scenes/Game/Animations/MoveCardAnimation.tsx
+++ src/Scenes/Game/Animations/MoveCardAnimation.tsx
@@ -1,12 +1,12 @@
-import { CSSProperties, useContext, useEffect, useReducer } from "react";
-import { getRectCenter, getRectCenterRight, rectIntersects } from "../../../Utils/Rect";
-import { useInterval, useTimeout } from "../../../Utils/UseInterval";
-import { CARD_SLOT_ID } from "../Pockets/CardSlot";
+import { CSSProperties, useReducer } from "react";
+import { Point, Rect, getRectCenter } from "../../../Utils/Rect";
+import { useInterval } from "../../../Utils/UseInterval";
 import CardView from "../CardView";
 import { Card, PocketRef } from "../Model/GameTable";
 import { Milliseconds } from "../Model/GameUpdate";
-import "./Style/MoveCardAnimation.css";
+import { CARD_SLOT_ID } from "../Pockets/CardSlot";
 import { CardTracker } from "./CardTracker";
+import "./Style/MoveCardAnimation.css";
 
 export interface MoveCardProps {
     tracker: CardTracker;
@@ -15,22 +15,41 @@ export interface MoveCardProps {
     duration: Milliseconds;
 }
 
+const CARD_HALF_W = 10;
+const POCKET_MIN_W = 50;
+
+function clampCardRect(cardRect: Rect, pocketRect: Rect | undefined): Point {
+    if (pocketRect && pocketRect.w > POCKET_MIN_W) {
+        if (cardRect.x < pocketRect.x) {
+            return {
+                x: pocketRect.x + CARD_HALF_W,
+                y: pocketRect.y + pocketRect.h / 2
+            };
+        } else if (cardRect.x + cardRect.w > pocketRect.x + pocketRect.w) {
+            return {
+                x: pocketRect.x + pocketRect.w - CARD_HALF_W,
+                y: pocketRect.y + pocketRect.h / 2
+            };
+        }
+    }
+    return getRectCenter(cardRect);
+}
+
 export default function MoveCardAnimation({ tracker, card, destPocket, duration }: MoveCardProps) {
     const [, forceUpdate] = useReducer(a => !a, false);
     useInterval(forceUpdate, 0, []);
 
-    const startRect = tracker.getTablePocket(card.pocket)?.getCardRect(CARD_SLOT_ID);
+    const startPocket = tracker.getTablePocket(card.pocket);
+    const startRect = startPocket?.getCardRect(CARD_SLOT_ID);
+
     const endPocket = tracker.getTablePocket(destPocket);
     const endRect = endPocket?.getCardRect(card.id) ?? startRect;
 
     // useEffect(() => endPocket?.scrollToEnd());
 
     if (startRect && endRect) {
-        const startPoint = getRectCenter(startRect);
-        
-        const endPocketRect = endPocket?.getPocketRect();
-        const endPoint = endPocketRect && !rectIntersects(endPocketRect, endRect)
-            ? getRectCenterRight(endPocketRect) : getRectCenter(endRect)
+        const startPoint = clampCardRect(startRect, startPocket?.getPocketRect());
+        const endPoint = clampCardRect(endRect, endPocket?.getPocketRect());
 
         const style = {
             '--startX': startPoint.x + 'px',
