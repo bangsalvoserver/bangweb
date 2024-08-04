996c43deb3f3c3a59f28c79d42271954d710e65c
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-08-04 17:41:57 +0200

    fix UnionUtils intellisense errors

--- src/Utils/UnionUtils.ts
+++ src/Utils/UnionUtils.ts
@@ -4,26 +4,34 @@ export type FilteredKeys<T, U> = { [P in keyof T]: T[P] extends U ? P : never }[
 export type SpreadUnion<T> = { [K in T as keyof K]: K[keyof K] };
 export type ChangeField<T, K extends keyof T, U> = { [Key in keyof T]: Key extends K ? U : T[Key]; };
 
-export function createUnionReducer<State, Update extends object, RetType = State>(functions: {
-    [K in Update as keyof K]: (this: State, value: K[keyof K]) => RetType;
-}) {
-    return (state: State, update: Update) => {
-        const [updateType, updateValue] = Object.entries(update)[0];
-        if (!(updateType in functions)) throw new Error("Invalid updateType: " + updateType);
+type UnionUpdate = Record<string, unknown>;
 
-        const fn = functions[updateType as keyof typeof functions];
-        return (fn as (this: State, update: unknown) => RetType).call(state, updateValue);
+type UnionReducerFunction<State, T, RetType> = (this: State, value: T) => RetType;
+type UnionReducerMap<State, Update extends UnionUpdate, RetType> = {
+    [K in Update as keyof K]: UnionReducerFunction<State, K[keyof K], RetType>
+};
+
+export function createUnionReducer<State, Update extends UnionUpdate, RetType = State>(functions: UnionReducerMap<State, Update, RetType>) {
+    return (state: State, update: Update) => {
+        const [updateType, updateValue] = Object.entries(update)[0] as [KeysOfUnion<Update>, unknown];
+        const fn = functions[updateType as KeysOfUnion<Update>] as UnionReducerFunction<State, unknown, RetType>;
+        if (!fn) throw new Error("Invalid updateType: " + updateType);
+
+        return fn.call(state, updateValue);
     };
 }
 
-export function createUnionDispatch<Update extends Object>(functions: {
-    [K in Update as keyof K]: (value: K[keyof K]) => void;
-}) {
-    return (update: Update) => {
-        const [updateType, updateValue] = Object.entries(update)[0];
-        if (!(updateType in functions)) throw new Error("Invalid updateType: " + updateType);
+type UnionDispatchFunction<T> = (value: T) => void;
+type UnionDispatchMap<Update extends UnionUpdate> = {
+    [K in Update as keyof K]: UnionDispatchFunction<K[keyof K]>
+};
 
-        const fn = functions[updateType as keyof typeof functions];
-        (fn as (update: unknown) => void)(updateValue);
+export function createUnionDispatch<Update extends UnionUpdate>(functions: UnionDispatchMap<Update>) {
+    return (update: Update) => {
+        const [updateType, updateValue] = Object.entries(update)[0] as [KeysOfUnion<Update>, unknown];
+        const fn = functions[updateType as KeysOfUnion<Update>] as UnionDispatchFunction<unknown>;
+        if (!fn) throw new Error("Invalid updateType: " + updateType);
+
+        fn(updateValue);
     };
 }
