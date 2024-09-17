0bbb9bf47610a1dfec218b557c66fa22052db84f
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-09-17 14:18:50 +0200

    fix PROMPT_SUICIDE

--- src/Locale/Italian/GameStrings.tsx
+++ src/Locale/Italian/GameStrings.tsx
@@ -13,7 +13,7 @@ export const GAME_STRINGS_ITALIAN: GameStringRegistry = {
     PROMPT_NO_BANGS_PLAYED: (card) =>                                       <>Non hai giocato nessun Bang, giocare {card} non avrebbe effetto. Continuare?</>,
     PROMPT_TARGET_SELF_GHOST_CARD: () =>                                    <>Stai scartando la tua carta fantasma. Continuare?</>,
     PROMPT_WASTEFUL_HEAL: (card, amount, max_amount) =>                     <>Giocare {card} per curarsi di {amount === 1 ? "un punto vita" : `${amount} punti vita`} sprecherebbe {max_amount === 1 ? "un punto vita" : `${max_amount} punti vita`}. Continuare?</>,
-    PROMPT_BANDIDOS_SUICIDE: (card) =>                                      <>Giocare {card} porta al suicidio. Continuare?</>,
+    PROMPT_SUICIDE: (card) =>                                               <>Giocare {card} porta al suicidio. Continuare?</>,
     PROMPT_DEATH: () =>                                                     <>Puoi giocare una birra per salvarti. Continuare?</>,
     PROMPT_PLAY_OR_PICK: (card) =>                                          <>{card} : Giocare la carta o scartarla?</>,
 
