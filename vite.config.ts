b6b2798d280ff2226d4e52e59390c46f8def9a76
Salvo Miosi <salvo.ilmiosi@gmail.com>
2024-09-23 18:51:05 +0200

    add tracking.html to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -6,5 +7,13 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), viteTsconfigPaths(), svgrPlugin()]
+  plugins: [react(), eslint(), viteTsconfigPaths(), svgrPlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        appGame: fileURLToPath(new URL('./index.html', import.meta.url)),
+        appTracking: fileURLToPath(new URL('./tracking.html', import.meta.url))
+      }
+    }
+  }
 });
