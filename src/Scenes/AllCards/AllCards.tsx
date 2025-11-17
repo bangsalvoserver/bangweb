ce8014eaac8542db162ddc406bea43eee22d7043
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-11-17 13:10:02 +0100

    add deck and language selects in all_cards

--- src/Scenes/AllCards/AllCards.tsx
+++ src/Scenes/AllCards/AllCards.tsx
@@ -1,6 +1,6 @@
-import { useMemo, useState } from "react";
+import { ChangeEvent, useMemo, useState } from "react";
 import '../../App.css';
-import Env from "../../Model/Env";
+import Env, { Language } from "../../Model/Env";
 import useFetch from "../../Utils/UseFetch";
 import { useMapRef } from "../../Utils/UseMapRef";
 import CardOverlayView from "../Game/CardOverlayView";
@@ -11,7 +11,8 @@ import { CardRef } from "../Game/Model/CardTracker";
 import { Card, getCardImage, KnownCard } from "../Game/Model/GameTable";
 import { SelectorConfirm, SelectorConfirmContext } from "../Game/Model/SelectorConfirm";
 import { OverlayState } from "../Game/Model/UseCardOverlay";
-import { useLanguage } from "../../Locale/Registry";
+import { LanguageProvider, useLanguage } from "../../Locale/Registry";
+import { DeckType } from "../Game/Model/CardEnums";
 
 function buildCard(cardData: CardData, index: number): Card {
     return {
@@ -25,14 +26,10 @@ function buildCard(cardData: CardData, index: number): Card {
     };
 }
 
-export default function AllCards() {
+function AllCardsInner({ deck }: { deck: DeckType }) {
     const language = useLanguage();
 
-    const params = new URLSearchParams(window.location.search);
-    const deck = params.get('deck') || 'main_deck';
-    const url = Env.bangCardsUrl + '/' + deck;
-
-    const cardDataList = useFetch<CardData[]>(url);
+    const cardDataList = useFetch<CardData[]>(Env.bangCardsUrl + '/' + deck);
     const cards = useMemo(() => (cardDataList ?? []).filter(card => card.image.length !== 0).map(buildCard), [cardDataList]);
 
     const cardRefs = useMapRef<number, CardRef>();
@@ -62,4 +59,47 @@ export default function AllCards() {
         </SelectorConfirmContext.Provider>
         <CardOverlayView overlayState={overlayState} />
     </div>
+}
+
+function getDefaultDeck(): DeckType {
+    const params = new URLSearchParams(window.location.search);
+    const deck = params.get('deck');
+    return deck ? deck as DeckType : 'main_deck';
+}
+
+export default function AllCards() {
+    const [deck, setDeck] = useState(getDefaultDeck);
+    const [language, setLanguage] = useState('en' as Language);
+
+    const handleChangeDeck = (event: ChangeEvent<HTMLSelectElement>) => {
+        setDeck(event.target.value as DeckType);
+    };
+
+    const handleChangeLanguage = (event: ChangeEvent<HTMLSelectElement>) => {
+        setLanguage(event.target.value as Language);
+    };
+
+    return <LanguageProvider selected={language}>
+        <div>
+            <select onChange={handleChangeDeck} className="m-2 p-1">
+                <option value='main_deck' selected={deck === 'main_deck'}>Main Deck</option>
+                <option value='character' selected={deck === 'character'}>Characters</option>
+                <option value='goldrush' selected={deck === 'goldrush'}>Gold Rush</option>
+                <option value='highnoon' selected={deck === 'highnoon'}>High Noon</option>
+                <option value='fistfulofcards' selected={deck === 'fistfulofcards'}>Fistful of Cards</option>
+                <option value='wildwestshow' selected={deck === 'wildwestshow'}>Wild West Show</option>
+                <option value='locomotive' selected={deck === 'locomotive'}>Locomotive</option>
+                <option value='train' selected={deck === 'train'}>Train Cards</option>
+                <option value='legends' selected={deck === 'legends'}>Legends</option>
+                <option value='feats' selected={deck === 'feats'}>Feats</option>
+                <option value='none' selected={deck === 'none'}>Hidden Deck</option>
+            </select>
+            <select onChange={handleChangeLanguage} className="m-2 p-1">
+                <option value='en' selected={language === 'en'}>English</option>
+                <option value='it' selected={language === 'it'}>Italian</option>
+                <option value='cs' selected={language === 'cs'}>Czech</option>
+            </select>
+        </div>
+        <AllCardsInner deck={deck} />
+    </LanguageProvider>
 }
--- src/all_cards.tsx
+++ src/all_cards.tsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AllCardsScene from './Scenes/AllCards/AllCards';
-import { LanguageProvider } from './Locale/Registry';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    <LanguageProvider>
-      <AllCardsScene />
-    </LanguageProvider>
+    <AllCardsScene />
   </StrictMode>
 );
