8579375bd93e796ffa4ef9327a06a22c855a88b7
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-11-23 20:22:58 +0100

    add chosen language indicator in LanguageMenu

--- src/Components/LanguageMenu.tsx
+++ src/Components/LanguageMenu.tsx
@@ -1,4 +1,5 @@
 import getLabel from "../Locale/GetLabel";
+import { useLanguage } from "../Locale/Registry";
 import { Language, LANGUAGES } from "../Model/Env";
 import { UserMenuItem } from "./UserMenu";
 
@@ -7,13 +8,15 @@ export interface LanguageProps {
 }
 
 export default function LanguageMenu({ setLanguage }: LanguageProps) {
+    const language = useLanguage();
     return <div className='user-menu z-50 py-2
         absolute top-10 right-0
         text-base list-none rounded-lg shadow bg-gray-700 divide-gray-600'
     >
-        {LANGUAGES.map(language =>
-            <UserMenuItem key={language} onClick={() => setLanguage(language)}>
-                {getLabel(language, 'ui', 'LANGUAGE_NAME')}
+        {LANGUAGES.map(lang =>
+            <UserMenuItem key={lang} onClick={() => setLanguage(lang)}>
+                {lang === language && <>● </>}
+                {getLabel(lang, 'ui', 'LANGUAGE_NAME')}
             </UserMenuItem>
         )}
     </div>
