49ba22767de6b92fceb826b1e203942390d1a07a
Salvo Miosi <salvo.ilmiosi@gmail.com>
2025-12-18 14:02:43 +0100

    optimize RecordUtils

--- src/Utils/RecordUtils.ts
+++ src/Utils/RecordUtils.ts
@@ -1,17 +1,23 @@
 export type UpdateFunction<T> = (value: T) => T;
 
-export type PartialRecord = { [k in keyof any]?: unknown };
+export type PartialRecord = Partial<Record<PropertyKey, unknown>>;
 
-export function removeById<T extends PartialRecord>(values: T, key: keyof T): T {
+export function removeById<T extends PartialRecord, K extends keyof T>(values: T, key: K): T {
     const { [key]: _, ...rest } = values;
     return rest as T;
 }
 
-export function removeByIds<T extends PartialRecord>(values: T, keys: (keyof T)[]): T {
-    return keys.reduce(removeById, values);
+export function removeByIds<T extends PartialRecord, K extends keyof T>(values: T, keys: K[]): T {
+    const result = { ...values };
+
+    for (const key of keys) {
+        delete result[key];
+    }
+
+    return result;
 }
 
-export function editById<T extends PartialRecord>(values: T, key: keyof T, mapper: UpdateFunction<T[keyof T]>): T {
+export function editById<T extends PartialRecord, K extends keyof T>(values: T, key: K, mapper: UpdateFunction<T[K]>): T {
     const newValue = mapper(values[key]);
     if (newValue === undefined) {
         return removeById(values, key);
@@ -20,6 +26,18 @@ export function editById<T extends PartialRecord>(values: T, key: keyof T, mappe
     }
 }
 
-export function editByIds<T extends PartialRecord>(values: T, keys: (keyof T)[], mapper: UpdateFunction<T[keyof T]>): T {
-    return keys.reduce((curValues, key) => editById(curValues, key, mapper), values);
+export function editByIds<T extends PartialRecord, K extends keyof T>(values: T, keys: K[], mapper: UpdateFunction<T[K]>): T {
+    const result = { ...values };
+
+    for (const key of keys) {
+        const newValue = mapper(result[key]);
+
+        if (newValue === undefined) {
+            delete result[key];
+        } else {
+            result[key] = newValue;
+        }
+    }
+
+    return result;
 }
