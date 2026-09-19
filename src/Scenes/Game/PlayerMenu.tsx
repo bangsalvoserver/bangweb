ed61d44289a3bfdcc30ae49a176165555717df46
Salvo Miosi <salvo.ilmiosi@gmail.com>
2026-09-19 16:08:34 +0200

    cleanup PlayerMenu css

--- src/Scenes/Game/PlayerMenu.tsx
+++ src/Scenes/Game/PlayerMenu.tsx
@@ -15,7 +15,7 @@ export default function PlayerMenu({ children }: PlayerMenuProps) {
         type="button" className="inline-flex items-center p-1 ml-1 text-sm rounded-lg focus:outline-none focus:ring-2 text-gray-400 bg-gray-900 hover:bg-gray-700 focus:ring-gray-600">
         { MENU_ICON }
     </button>
-    { isMenuOpen && <div className='user-menu z-50 py-2
+    { isMenuOpen && <div className='z-50 py-2
         absolute top-10 right-0
         text-base list-none rounded-lg shadow bg-gray-700 divide-gray-600'>
             { children }
